53615562039c0c0d297c3c8fb97b8015dd71fce4
GeorgeCiachir <george.ciachir@yahoo.com>
2022-04-05 07:51:13 +0300

    first commit

---
+++ src/main/java/com/georgeciachir/az204instrumentsolutionsformonitoringandloggind/Az204InstrumentSolutionsForMonitoringAndLoggindApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.az204instrumentsolutionsformonitoringandloggind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Az204InstrumentSolutionsForMonitoringAndLoggindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Az204InstrumentSolutionsForMonitoringAndLoggindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/georgeciachir/az204instrumentsolutionsformonitoringandloggind/Az204InstrumentSolutionsForMonitoringAndLoggindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.az204instrumentsolutionsformonitoringandloggind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Az204InstrumentSolutionsForMonitoringAndLoggindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
